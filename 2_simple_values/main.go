56b63a506b4e2671ee33aca86b27bb29be43ad5c
sohel syed <mr.sohelsyed@gmail.com>
2025-08-09 09:43:10 +0530

    simple values in go

---
+++ 2_simple_values/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+
+func main(){
+	// simple values
+
+	// Integers
+	fmt.Println(1)
+
+	// Strings
+	fmt.Println("Hello World")
+
+	// Booleans
+	fmt.Println(true)
+	fmt.Println(false)
+
+	// Floats
+	fmt.Println(1.0)
+
+}
