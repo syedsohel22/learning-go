e250c2a972033efc6dbb5dabb9eec436fa455ae4
sohel syed <mr.sohelsyed@gmail.com>
2025-08-09 10:00:23 +0530

    constants

---
+++ constants/constants.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main(){
+	const name string  = "sohel"
+
+	// cannot assign to name (neither addressable nor a map index expression)compilerUnassignableOperand
+	// name = "javascript"  
+	
+	
+	// you can't you shorthand := out side function
+
+	// constant grouping
+
+	const(
+		port =3000
+		host = "localhost"
+	)
+
+	fmt.Println(name,port,host)
+}
