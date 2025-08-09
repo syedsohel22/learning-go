a1ec979d3425fcbda67cc788fca87b006a44cda4
sohel syed <mr.sohelsyed@gmail.com>
2025-08-09 09:36:37 +0530

    hellow world

---
+++ 1_hellow_world/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("hello world")
+}
+
+// for running go code
+
+// 1. Build and run on terminal
+// go build <file path>
+// ./<exacutable binary>
+
+// 2. Directly run 
+// go run <file path>
