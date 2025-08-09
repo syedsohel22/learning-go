50c157f9370e92485f7f57f3b47a02ac0ae2b9b7
sohel syed <mr.sohelsyed@gmail.com>
2025-08-09 10:57:27 +0530

    if else

---
+++ 6_if_else/if_else.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+
+func main(){
+	age:=17
+
+	if age>=18{
+		fmt.Println("Person is an adult")
+	}else if age>=15{
+		fmt.Println("Person is a teenager")
+	}else{
+		fmt.Println("Person is a child")
+	}
+
+	// in go we dont use () in loop and if else
+
+
+	// Logical operators
+	// || OR operator
+	// && AND operator
+	// ! NOT operator
+
+	// we can declare a variable in if construct
+
+	// go does not have ternary as of go 1.22
+}
