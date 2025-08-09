ddedbf5a915cf3212f5a5646d9b377b607c97447
sohel syed <mr.sohelsyed@gmail.com>
2025-08-09 09:52:26 +0530

    go variables

---
+++ 3_variables/variables.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+
+func main() {
+	var name string = "golang"
+
+	fmt.Println(name)
+
+	// go lang infers the datatype of variable
+	var name1 ="golang-1"
+	fmt.Println(name1)
+
+	// shorthand sysntax
+	// when you want to declare and assign value to variable
+	name2 := "golang-2"
+	fmt.Println(name2)
+
+	
+
+}  
