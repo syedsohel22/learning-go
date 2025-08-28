8df42fce3834b99b4502a9aea073c4b13c9fae06
sohel syed <mr.sohelsyed@gmail.com>
2025-08-28 23:19:55 +0530

    range in go, arrays, maps and string

--- coders_gyan_go/11_range/range.go
+++ coders_gyan_go/11_range/range.go
@@ -1,5 +1,35 @@
 package main
 
+import "fmt"
+// interating over data structures
 func main() {
 
+ nums:= []int {6,7,8}
+
+ for i:=0;i<len(nums);i++{
+	fmt.Println(nums[i])
+ }
+
+ sum:=0
+for i,num := range(nums){
+	fmt.Println(num,i)
+	sum=sum+num
+}
+
+fmt.Println(sum)
+
+
+m:= map[string]string{"fname":"john","lname":"doe"}
+
+for k,v:=range m {
+	fmt.Println(k,v)
+}
+
+for i,c:= range "golang"{
+	// here c will return unicode code 
+	// point rune
+	// i is starting point (byte) of rune
+	// fmt.Println(i,c)
+	fmt.Println(i, c,string(c))
+}
 }
